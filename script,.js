3999cfeae5b805432a2ead26a9d776f37c71c0ba
ArmenHakobyan <132355240+Hakoobyan@users.noreply.github.com>
2024-05-17 04:41:46 +0400

    Update script,.js

--- script,.js
+++ script,.js
@@ -1,24 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var menuIcon = document.getElementById('menu-icon');
+    var menu = document.getElementById('menu');
+
+    menuIcon.addEventListener('click', function () {
+        document.body.classList.toggle('menu-open');
+    });
+
+    window.closeWindowAndMenu = function () {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('open');
+        document.body.classList.remove('menu-open');
+    };
+
+});
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
 };
 
 window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navbar.classList.remove('open');
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
 };
 
 function closeWindowAndMenu() {
     menu.classList.remove('bx-x');
-	navbar.classList.remove('open');
+    navbar.classList.remove('open');
 }
 
 
 window.onscroll = function () {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         document.getElementById("myButton").className = "show";
     } else {
         document.getElementById("myButton").className = "";
